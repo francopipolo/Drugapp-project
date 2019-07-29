854cc34f3a172595ff212a36859bfaef2c25452b
Franco <franco.pipolo393@gmail.com>
2019-07-29 00:52:56 -0300

    inicio del proyecto

---
+++ app/src/main/java/com/example/drugapp/LoginActivity.java
@@ -0,0 +1,26 @@
+package com.example.drugapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        Button btn = (Button) findViewById(R.id.btnlogin);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent (v.getContext(), MainActivity.class);
+                startActivityForResult(intent, 0);
+            }
+        });
+    }
+}
